c6b3e39fe7db7ca6dfca39026f35ff39cfd1dcc5
nick357 <nick357@qq.com>
2017-03-06 16:20:38 +0800

    替换： light-locker -> dm-tool lock

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 # /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 nm-applet &
 pkill xss-lock
-xss-lock -- light-locker &
+xss-lock -- dm-tool lock &
 pkill indicator-keylock
 indicator-keylock &
 pkill volumeicon
